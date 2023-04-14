3dcedfc437f70cbc600b90ebdc29767be4b4f343
rvronski <rvronski@gmail.com>
2023-04-14 21:38:01 +0500

    Initial Commit

---
+++ IPTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IPTest
+//
+//  Created by ROMAN VRONSKY on 14.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
